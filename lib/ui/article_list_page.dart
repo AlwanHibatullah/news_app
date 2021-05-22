14fc8298b3355ff3064e5d994f9e4bd9a9376f81
Alwan Hibatullah <alwanhibatullah@gmail.com>
2021-05-22 21:13:27 +0700

    Add provider

--- lib/ui/article_list_page.dart
+++ lib/ui/article_list_page.dart
@@ -1,49 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/data/api/api_service.dart';
-import 'package:news_app/data/model/article.dart';
+import 'package:news_app/provider/news_provider.dart';
 import 'package:news_app/widgets/card_article.dart';
 import 'package:news_app/widgets/platform_widget.dart';
+import 'package:provider/provider.dart';
 
-class ArticleListPage extends StatefulWidget {
-  @override
-  _ArticleListPageState createState() => _ArticleListPageState();
-}
-
-class _ArticleListPageState extends State<ArticleListPage> {
-  late Future<ArticlesResult> _article;
-
-  @override
-  void initState() {
-    super.initState();
-    _article = ApiService().topHeadlines();
-  }
-
+class ArticleListPage extends StatelessWidget{
   Widget _buildList(BuildContext context) {
-    return FutureBuilder(
-      future: _article,
-      builder: (context, AsyncSnapshot<ArticlesResult> snapshot) {
-        var state = snapshot.connectionState;
-        if (state != ConnectionState.done) {
+    return Consumer<NewsProvider>(
+      builder: (context, state, _) {
+        if (state.state == ResultState.Loading) {
           return Center(
             child: CircularProgressIndicator(),
           );
+        } else if (state.state == ResultState.HasData) {
+          return ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              var article = state.result.articles[index];
+              return CardArticle(article: article);
+            },
+            itemCount: state.result.articles.length,
+          );
+        } else if (state.state == ResultState.NoData) {
+          return Center(
+            child: Text(state.message),
+          );
+        } else if (state.state == ResultState.Error) {
+          return Center(
+            child: Text(state.message),
+          );
         } else {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                var article = snapshot.data?.articles[index];
-                return CardArticle(article: article!);
-              },
-              itemCount: snapshot.data?.articles.length,
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          } else {
-            return Text('');
-          }
+          return Center(
+            child: Text(''),
+          );
         }
       },
     );
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:news_app/data/api/api_service.dart';
+import 'package:news_app/provider/news_provider.dart';
 import 'package:news_app/ui/settings_page.dart';
 import 'package:news_app/ui/article_list_page.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/widgets/platform_widget.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   static const routeName = '/home_page';
@@ -25,7 +28,12 @@ class _HomePageState extends State<HomePage> {
         label: 'Settings')
   ];
 
-  List<Widget> _listWidget = [ArticleListPage(), SettingsPage()];
+  List<Widget> _listWidget = [
+    ChangeNotifierProvider<NewsProvider>(
+      create: (_) => NewsProvider(apiService: ApiService()),
+      child: ArticleListPage(),
+      ), 
+    SettingsPage()];
 
   @override
   Widget build(BuildContext context) {
